48dad3aced2cde929a168dafc27124297bdf338e
Brenna Lavin <lavinbrenna@gmail.com>
2022-01-13 10:05:12 -0800

    add branching conditional statement
    Co-Authored-By: Ryan Spencer <ryan.spencer1220@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
   const height = parseInt(prompt("How tall are you in inches?"));
-
-  /*if(height > 54){
-   
-  }else if(){
-
-  }else if(){
-
-  }else if(){
-
-  }else if(){
-
-  }else{
-
+  if(height >= 53){
+   $('#allHeights').show();
+   $('#thirtySix').show();
+   $('#fortyTwo').show();
+   $('#fortyEight').show();
+   $('#fiftyThree').show();
   }
-  */
+  else if(height <= 53 && height > 42) {
+    $('#allHeights').show();
+    $('#thirtySix').show();
+    $('#fortyTwo').show();
+    $('#fortyEight').show();
+    $('#underFiftyFour').show()
+  }
+  else if(height <= 42) {
+    $('#allHeights').show();
+    $('#thirtySix').show();
+    $('#fortyTwo').show();
+    $('#underFiftyFour').show()
+  }
+  else if(height <= 36) {
+    $('#allHeights').show();
+    $('#thirtySix').show();
+    $('#underFiftyFour').show()
+  }
+  else{
+  $('#allHeights').show();
+}
 });
