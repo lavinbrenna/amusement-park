87fbf9cb1e1933f07c491f71775b4457a795722c
Brenna Lavin <lavinbrenna@gmail.com>
2022-01-13 09:27:02 -0800

    add boilerplate functionality, imgs, bootstrap, jQuery
    Co-Authored-By: Ryan Spencer <ryan.spencer1220@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  const height = parseInt(prompt("How tall are you in inches?"));
+
+  /*if(height > 54){
+   
+  }else if(){
+
+  }else if(){
+
+  }else if(){
+
+  }else if(){
+
+  }else{
+
+  }
+  */
+});
